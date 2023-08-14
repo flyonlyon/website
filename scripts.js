2e53764a4baec46260d2da95fd12a3c85aa943ea
flyon <flyon@umich.edu>
2023-08-14 00:21:16 -0400

    polished version

--- scripts.js
+++ scripts.js
@@ -1,19 +1,3 @@
-// Initializes our page upon load
-window.onload = function() { 
-    changeHues();
-} // window.onload
-
-function changeHues() {
-    changeSize(document.getElementById("green"));
-    changeSize(document.getElementById("purple"));
-    changeSize(document.getElementById("brown"));
-} // changeHues()
-
-// Randomly the size of the hue circles
-function changeSize(circle) {
-    var change = Math.random() - 0.5;
-    circle.style.width += change;
-    circle.style.height += change;
-    circle.style.paddingTop -= change;
-    circle.style.paddingLeft -= change;
-} // changeSize
+function openLink(string url) {
+    window.open(url, '_blank');
+}
